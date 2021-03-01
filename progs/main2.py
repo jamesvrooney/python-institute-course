ad6050f0373276f6f060e03fadaa98df547277c5
jamesvrooney <jamesvrooney@hotmail.com>
2021-03-01 22:17:46 +0000

    Imported funT from Tau module.

--- progs/main2.py
+++ progs/main2.py
@@ -3,7 +3,8 @@ path.append('../packages')
 
 import extra.iota
 import extra.good.best.sigma as sigma
+from extra.good.best.tau import funT
 
 print(extra.iota.funI())
 print(sigma.funS())
-
+print(funT())
