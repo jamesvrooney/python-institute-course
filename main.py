91becc16e71d99f8befc495bc01bcd645ac9175e
jamesvrooney <jamesvrooney@hotmail.com>
2021-03-05 12:11:22 +0000

    Test for very simple class.

---
+++ main.py
@@ -0,0 +1,5 @@
+from simple_class import TheSimplestClass
+
+my_first_object = TheSimplestClass()
+
+my_first_object.my_name()
---
+++ simple_class.py
@@ -0,0 +1,3 @@
+class TheSimplestClass:
+    def my_name(self):
+        print("My name is James")
